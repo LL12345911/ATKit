62783bd443b900b5fbe5499a30fd8de9ab40446e
Mars <sky12345911@163.com>
2020-09-18 13:44:05 +0800

    no message

--- ATKitLib/Share/OpenShareManager.h
+++ ATKitLib/Share/OpenShareManager.h
@@ -13,16 +13,33 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface OpenShareManager : NSObject
 
-///  邀请加入
+///  分享
 /// @param linkUrl link
 /// @param desc 详细说明
+/// @param imageName 图片名
 + (void)invitedToJoinUs:(NSString *)linkUrl desc:(NSString *)desc image:(NSString *)imageName;
 
-//+ (void)shareMessage_LinkUrl:(NSString *)linkUrl title:(NSString *)title;
+
+/// 分享
+/// @param linkUrl 链接
+/// @param desc 描述
+/// @param imageName 图片名
+/// @param excludetypes 例外（不支持）
++ (void)invitedToJoinUs:(NSString *)linkUrl desc:(NSString *)desc image:(NSString *)imageName excludetypes:(NSArray *)excludetypes;
 
 
+/// 分享
+/// @param linkUrl 链接
+/// @param title 描述
+/// @param image  UIImage 图片名
 + (void)shareMessage_LinkUrl:(NSString *)linkUrl title:(NSString *)title image:(UIImage *)image;
 
+/// 分享
+/// @param linkUrl 链接
+/// @param title 描述
+/// @param image  UIImage 图片名
+/// @param excludetypes 例外（不支持）
++ (void)shareMessage_LinkUrl:(NSString *)linkUrl title:(NSString *)title image:(UIImage *)image excludetypes:(NSArray *)excludetypes;
 
 //邀请加入
 + (void)sharePDF:(NSString *)linkUrl;
--- ATKitLib/Share/OpenShareManager.m
+++ ATKitLib/Share/OpenShareManager.m
@@ -40,17 +40,68 @@
     return excludeTypesM;
 }
 
-//邀请加入
+///  分享
+/// @param linkUrl link
+/// @param desc 详细说明
+/// @param imageName 图片名
 + (void)invitedToJoinUs:(NSString *)linkUrl desc:(NSString *)desc image:(NSString *)imageName{
     
     NSString *shareTitle = [NSString stringWithFormat:@"%@",desc];;
     UIImage *shareImage = [UIImage imageNamed:imageName];
     NSURL *shareUrl = [NSURL URLWithString:linkUrl];
-    NSArray *activityItems = @[@"",shareTitle,shareImage,shareUrl];
+    NSArray *activityItems = @[shareTitle,shareImage,shareUrl];
     
     UIActivity *actice = [[UIActivity alloc] init];
      NSArray *activities = @[actice];
     
+    [self pushActivityViewControllerWithActivityItems:activityItems applicationActivities:activities excludetypes:nil];
+    
+//    UIActivityViewController * activityVC = [[UIActivityViewController alloc] initWithActivityItems:activityItems applicationActivities:activities];
+//
+//    UIActivityViewControllerCompletionWithItemsHandler myBlock = ^(UIActivityType __nullable activityType, BOOL completed, NSArray * __nullable returnedItems, NSError * __nullable activityError){
+//        if (completed) {
+//            //分享完成
+//            [Toast showWithText:@"分享成功"];
+//        }else{
+//            //分享失败
+//            [Toast showWithText:@"分享失败"];
+//        }
+//
+//        [activityVC dismissViewControllerAnimated:YES completion:nil];
+//    };
+//
+//    activityVC.completionWithItemsHandler = myBlock;
+//    // 设定不想显示的平台和功能
+//    activityVC.excludedActivityTypes = [self excludetypes];
+//    //这儿一定要做iPhone与iPad的判断，因为这儿只有iPhone可以present，iPad需pop，所以这儿actVC.popoverPresentationController.sourceView = self.view;在iPad下必须有，不然iPad会crash，self.view你可以换成任何view，你可以理解为弹出的窗需要找个依托。
+//    activityVC.modalPresentationStyle = UIModalPresentationFullScreen;
+//    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
+//        activityVC.popoverPresentationController.sourceView = [OpenShareManager currentVC].view;
+//        [[OpenShareManager currentVC] presentViewController:activityVC animated:YES completion:nil];
+//    } else {
+//        [[OpenShareManager currentVC] presentViewController:activityVC animated:YES completion:nil];
+//    }
+}
+
+/// 分享
+/// @param linkUrl 链接
+/// @param desc 描述
+/// @param imageName 图片名
+/// @param excludetypes 例外（不支持）
++ (void)invitedToJoinUs:(NSString *)linkUrl desc:(NSString *)desc image:(NSString *)imageName excludetypes:(NSArray *)excludetypes{
+    NSString *shareTitle = [NSString stringWithFormat:@"%@",desc];;
+    UIImage *shareImage = [UIImage imageNamed:imageName];
+    NSURL *shareUrl = [NSURL URLWithString:linkUrl];
+    NSArray *activityItems = @[shareTitle,shareImage,shareUrl];
+    
+    UIActivity *actice = [[UIActivity alloc] init];
+     NSArray *activities = @[actice];
+    
+    [self pushActivityViewControllerWithActivityItems:activityItems applicationActivities:activities excludetypes:excludetypes];
+    
+}
+
++ (void)pushActivityViewControllerWithActivityItems:(NSArray *)activityItems applicationActivities:(NSArray *)activities excludetypes:(NSArray *)excludetypes{
     UIActivityViewController * activityVC = [[UIActivityViewController alloc] initWithActivityItems:activityItems applicationActivities:activities];
     
     UIActivityViewControllerCompletionWithItemsHandler myBlock = ^(UIActivityType __nullable activityType, BOOL completed, NSArray * __nullable returnedItems, NSError * __nullable activityError){
@@ -67,7 +118,7 @@
     
     activityVC.completionWithItemsHandler = myBlock;
     // 设定不想显示的平台和功能
-    activityVC.excludedActivityTypes = [self excludetypes];
+    activityVC.excludedActivityTypes = excludetypes ? excludetypes : [self excludetypes];
     //这儿一定要做iPhone与iPad的判断，因为这儿只有iPhone可以present，iPad需pop，所以这儿actVC.popoverPresentationController.sourceView = self.view;在iPad下必须有，不然iPad会crash，self.view你可以换成任何view，你可以理解为弹出的窗需要找个依托。
     activityVC.modalPresentationStyle = UIModalPresentationFullScreen;
     if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
@@ -78,15 +129,11 @@
     }
 }
 
-////邀请加入
-//+ (void)shareMessage_LinkUrl:(NSString *)linkUrl title:(NSString *)title{
-//
-//    UIImage *shareImage = [UIImage imageNamed:@"denglu_logo"];
-//    [self shareMessage_LinkUrl:linkUrl title:title image:shareImage];
-//
-//}
 
-
+/// 分享
+/// @param linkUrl 链接
+/// @param title 描述
+/// @param image  UIImage 图片名
 + (void)shareMessage_LinkUrl:(NSString *)linkUrl title:(NSString *)title image:(UIImage *)image{
     NSURL *shareUrl = [NSURL URLWithString:linkUrl];
     NSArray *activityItems;
@@ -98,31 +145,52 @@
     //    UIActivity *actice = [[UIActivity alloc] init];
     //    NSArray *activities = @[actice];
     
-    UIActivityViewController *activityVC = [[UIActivityViewController alloc] initWithActivityItems:activityItems applicationActivities:nil];
+    [self pushActivityViewControllerWithActivityItems:activityItems applicationActivities:nil excludetypes:nil];
+
     
-    UIActivityViewControllerCompletionWithItemsHandler myBlock = ^(UIActivityType __nullable activityType, BOOL completed, NSArray * __nullable returnedItems, NSError * __nullable activityError){
-        if (completed) {
-            //分享完成
-            [Toast showWithText:@"分享成功"];
-        }else{
-            //分享失败
-            [Toast showWithText:@"分享失败"];
-        }
-        
-        [activityVC dismissViewControllerAnimated:YES completion:nil];
-    };
-    
-    activityVC.completionWithItemsHandler = myBlock;
-    // 设定不想显示的平台和功能
-    activityVC.excludedActivityTypes = [self excludetypes];
-    //这儿一定要做iPhone与iPad的判断，因为这儿只有iPhone可以present，iPad需pop，所以这儿actVC.popoverPresentationController.sourceView = self.view;在iPad下必须有，不然iPad会crash，self.view你可以换成任何view，你可以理解为弹出的窗需要找个依托。
-    activityVC.modalPresentationStyle = UIModalPresentationFullScreen;
-    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
-        activityVC.popoverPresentationController.sourceView = [OpenShareManager currentVC].view;
-        [[OpenShareManager currentVC] presentViewController:activityVC animated:YES completion:nil];
-    } else {
-        [[OpenShareManager currentVC] presentViewController:activityVC animated:YES completion:nil];
+//    UIActivityViewController *activityVC = [[UIActivityViewController alloc] initWithActivityItems:activityItems applicationActivities:nil];
+//
+//    UIActivityViewControllerCompletionWithItemsHandler myBlock = ^(UIActivityType __nullable activityType, BOOL completed, NSArray * __nullable returnedItems, NSError * __nullable activityError){
+//        if (completed) {
+//            //分享完成
+//            [Toast showWithText:@"分享成功"];
+//        }else{
+//            //分享失败
+//            [Toast showWithText:@"分享失败"];
+//        }
+//
+//        [activityVC dismissViewControllerAnimated:YES completion:nil];
+//    };
+//
+//    activityVC.completionWithItemsHandler = myBlock;
+//    // 设定不想显示的平台和功能
+//    activityVC.excludedActivityTypes = [self excludetypes];
+//    //这儿一定要做iPhone与iPad的判断，因为这儿只有iPhone可以present，iPad需pop，所以这儿actVC.popoverPresentationController.sourceView = self.view;在iPad下必须有，不然iPad会crash，self.view你可以换成任何view，你可以理解为弹出的窗需要找个依托。
+//    activityVC.modalPresentationStyle = UIModalPresentationFullScreen;
+//    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
+//        activityVC.popoverPresentationController.sourceView = [OpenShareManager currentVC].view;
+//        [[OpenShareManager currentVC] presentViewController:activityVC animated:YES completion:nil];
+//    } else {
+//        [[OpenShareManager currentVC] presentViewController:activityVC animated:YES completion:nil];
+//    }
+}
+/// 分享
+/// @param linkUrl 链接
+/// @param title 描述
+/// @param image  UIImage 图片名
+/// @param excludetypes 例外（不支持）
++ (void)shareMessage_LinkUrl:(NSString *)linkUrl title:(NSString *)title image:(UIImage *)image excludetypes:(NSArray *)excludetypes{
+    NSURL *shareUrl = [NSURL URLWithString:linkUrl];
+    NSArray *activityItems;
+    if (image) {
+       activityItems = @[kIfNull(title),image,shareUrl];
+    }else {
+       activityItems = @[kIfNull(title),shareUrl];
     }
+    //    UIActivity *actice = [[UIActivity alloc] init];
+    //    NSArray *activities = @[actice];
+    [self pushActivityViewControllerWithActivityItems:activityItems applicationActivities:nil excludetypes:excludetypes];
+
 }
 
 
